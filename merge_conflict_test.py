57a40e11bbbcf6c5eb7cc4155be92a0ca8ade31a
Nithin shetty <shettynithin91@gmail.com>
2026-07-07 19:01:45 +0530

    feature update

---
+++ merge_conflict_test.py
@@ -0,0 +1,6 @@
+# Simple file for testing Git merge conflicts
+# Change the same line on two different branches, then merge them.
+
+message = "This line will cause a merge conflict when edited on two branches , new changes"
+
+print(message)
